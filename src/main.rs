738d9f9fb27d9502c1e5c224a72f8702251d20ea
mikemehl <michaelalanmehl@gmail.com>
2020-09-29 23:44:46 -0400

    Initial things woooooooo.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod ChipEight;
+
 fn main() {
+    let mut em = ChipEight::new_em();
     println!("Hello, world!");
+    let fname = "/mnt/c/Users/micha/Dropbox/Code/CHIP8_Rust/chip8_roms/test_opcode.ch8";
+    em.load_cart(fname.to_string());
 }
