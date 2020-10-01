c5200b9968788f0b41922dbbc14617fefbd3e970
mikemehl <michaelalanmehl@gmail.com>
2020-09-30 23:16:26 -0400

    Added some opcode decoding.

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,10 @@ fn main() {
     println!("Hello, world!");
     let fname = "/mnt/c/Users/micha/Dropbox/Code/CHIP8_Rust/chip8_roms/test_opcode.ch8";
     em.load_cart(fname.to_string());
+
+    println!("Starting emulation...");
+    loop
+    {
+       em.cycle();
+    }
 }
